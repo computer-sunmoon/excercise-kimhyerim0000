34a5fd6ada8eb0ca9f077327792d5cea91de76c1
kimhy <kimhyyefla@gmail.com>
2025-09-04 14:25:55 +0900

    Fix: Corrected list elements and added reduce function example

--- lib/week01/1.3/5_reduce_function.dart
+++ lib/week01/1.3/5_reduce_function.dart
@@ -1,3 +1,8 @@
 void main(){
-  List<String> list=['제니','로제','리사','']
+  List<String> list=['제니','로제','리사','지수'];
+
+  final newList=list.reduce(
+    (value,element)=> value+' , '+element
+  );
+
 }
