df1abe6233b20c40ad0d842749d524936ae5701f
kimhy <kimhyyefla@gmail.com>
2025-09-04 14:15:55 +0900

    Fix: Use 'final' for newList and print original list

--- lib/week01/1.3/4_map_function.dart
+++ lib/week01/1.3/4_map_function.dart
@@ -1,9 +1,10 @@
 void main(){
   List<String> list=['제니','리사','지수','로제'];
 
-  newList=list.map(
+  final newList=list.map(
     (name)=> '블랙핑크 $name',
   );
   print(newList);
 
+  print(list.toList());
 }
