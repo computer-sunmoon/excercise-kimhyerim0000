1b3cdbaa71eb1f47066ff0f8d55b741a354bd44e
kimhy <kimhyyefla@gmail.com>
2025-09-04 14:27:55 +0900

    feat: Add comments explaining Iterable return type in map and where functions

--- lib/week01/1.3/3_where_function.dart
+++ lib/week01/1.3/3_where_function.dart
@@ -2,7 +2,7 @@ void main(){
   List<String> list=['제니','리사','지수','로제'];
 
   final newList=list.where(
-    (name) => name=='제니' || name=='리사',
+    (name) => name=='제니' || name=='리사', //얘도 Iterable
   );
   
   print(newList);
--- lib/week01/1.3/4_map_function.dart
+++ lib/week01/1.3/4_map_function.dart
@@ -2,7 +2,7 @@ void main(){
   List<String> list=['제니','리사','지수','로제'];
 
   final newList=list.map(
-    (name)=> '블랙핑크 $name', //Iterable
+    (name)=> '블랙핑크 $name', //Iterable이라서 ,
   );
   print(newList);
 
