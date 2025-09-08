34ecbe18c7dda0209796fe11e4ed5d2a74efe7ab
kimhy <kimhyyefla@gmail.com>
2025-09-08 09:07:47 +0900

    feat: Add basic and null operators examples

--- lib/week01/1.5/1_basic_operators.dart
+++ lib/week01/1.5/1_basic_operators.dart
@@ -4,5 +4,12 @@ void main() {
   print(number-2);
   print(number*2);
   print(number/2);
-  print(number%3);
+  print(number %3);
+
+  number++;
+  number--;
+  number+=2;
+  number-=2;
+  number*=2;
+  number/=2;
 }
