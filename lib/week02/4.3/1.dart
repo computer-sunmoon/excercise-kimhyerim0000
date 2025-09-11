392580ab4fdc39f941155039060f4ed14dada0ac
kimhy <kimhyyefla@gmail.com>
2025-09-11 13:57:44 +0900

    feat(week02): Implement Korean to English day name conversion using switch

--- lib/week02/4.3/1.dart
+++ lib/week02/4.3/1.dart
@@ -2,6 +2,15 @@ void main(){
   String daykor='월요일';
 
   String dayEnglish=switch(daykor) {
-    
-  }
+    '월요일'=>'Monday',
+    '화요일'=>'Tuesday',
+    '수요일'=>'Wednesday',
+    '목요일'=>'Thursday',
+    '금요일'=>'Friday',
+    '토오일'=>'Saturday',
+    '일요일'=>'Sunday',
+    _=>'Not Found',
+  };
+
+  print(dayEnglish);
 }
