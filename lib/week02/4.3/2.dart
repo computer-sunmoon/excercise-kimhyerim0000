158a06d7e35ecf4e9dbb7242df56a117ecf846d2
kimhy <kimhyyefla@gmail.com>
2025-09-11 14:00:15 +0900

    feat: Implement switcher function with pattern matching

---
+++ lib/week02/4.3/2.dart
@@ -0,0 +1,14 @@
+void switcher(dynamic anything){
+  switch(anything){
+    case 'aaa':
+    print('match:aaa');
+    break;
+
+    case [1,2]:
+    print('match:[1,2]');
+    break;
+
+    case [_,_,_]:
+    print('match:[_,]')
+  }
+}
