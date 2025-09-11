763da6fd8ed6b95e1c173d9c6e4f3ab145ee31a4
kimhy <kimhyyefla@gmail.com>
2025-09-11 14:56:21 +0900

    feat(imsi): Implement getter, setter, and sayName methods for Idol class

--- lib/week02/5/imsi.dart
+++ lib/week02/5/imsi.dart
@@ -4,5 +4,14 @@ class Idol{
 
   Idol(this.name,this.members);
 
-  int get getter
+  int get getter(){
+    return this.members;
+  }
+  set setter(setmembers){
+    this.members=setmembers;
+  }
+
+  void sayName(){
+    print('저는 $name입니다.');
+  }
 }
