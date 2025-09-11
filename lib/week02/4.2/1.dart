11f4c9369c48b3cba5b09fedb2e5e7bd643667cc
kimhy <kimhyyefla@gmail.com>
2025-09-11 13:13:14 +0900

    feat(week02/4.2): Use destructuring for concise variable assignment

--- lib/week02/4.2/1.dart
+++ lib/week02/4.2/1.dart
@@ -1,4 +1,8 @@
 void main(){
+  // 아래 코드와 같지만 구조 분해를 사용하면 한 줄에 해결할 수 있습니다.
+  // final newJeans=['민지','해린'];
+  // print(newJeans[0]);
+  // print(newJeans[1]);
   final [minji,haerin] = ['민지','해린'];
   print(minji);
   print(haerin);
