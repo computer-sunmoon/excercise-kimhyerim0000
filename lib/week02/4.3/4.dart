cb01aa88bafcdb319c9a6020d7963163204eadbe
kimhy <kimhyyefla@gmail.com>
2025-09-11 14:27:45 +0900

    Fix: Use positional field accessors in switch case guard

--- lib/week02/4.3/4.dart
+++ lib/week02/4.3/4.dart
@@ -2,6 +2,11 @@ void main(){
   (int a,int b) imsi=(1,-1);
 
   switch(imsi){
-    case (1,_) when imsi.b>
+    case (1,_) when imsi.$2>0:
+    print('1,_');
+    break;
+    
+    default:
+    print('default');
   }
 }
