e49a931547e7b4e52c9734f1de5dc1335e1b186b
kimhy <kimhyyefla@gmail.com>
2025-09-11 14:24:45 +0900

    Fix: Handle null bool in switch statement.

--- lib/week02/4.3/3.dart
+++ lib/week02/4.3/3.dart
@@ -1,15 +1,13 @@
 void main(){
   // tf에 입력될 수 있는 값은 true
-  bool? tf=true;
+  bool? tf;
 
   switch(tf){
     case true:
     print('true');
-    break;
 
     case false:
     print('false');
-    break;
 
     default: 
     print('null');
