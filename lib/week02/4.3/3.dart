6886a9e1b170cfa3c069cc3e3920a3ed079f9ccb
kimhy <kimhyyefla@gmail.com>
2025-09-11 14:23:45 +0900

    feat: Implement nullable boolean switch statement

---
+++ lib/week02/4.3/3.dart
@@ -0,0 +1,16 @@
+void main(){
+  bool? tf;
+
+  switch(tf){
+    case true:
+    print('true');
+    break;
+
+    case false:
+    print('false');
+    break;
+
+    default: 
+    print('null');
+  }
+}
