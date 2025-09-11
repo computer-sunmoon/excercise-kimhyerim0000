e548494979d1f9e9327d6822fdb493b242cd7ebd
kimhy <kimhyyefla@gmail.com>
2025-09-11 15:25:21 +0900

    Fix: Correct setter usage and improve readability

--- lib/week02/5/imsi.dart
+++ lib/week02/5/imsi.dart
@@ -1,36 +1,43 @@
-class Idol{
+class Idol {
   String name;
   int _members;
 
-  Idol(this.name,this._members);
+  Idol(this.name, this._members);
 
-  int get getter{
+  int get getter {
     return this._members;
   }
-  set setter(setmembers){
-    if(setmembers>0 && setmembers<13){this._members=setmembers;}
-    else {print('멤버 수는 1~12 사이어야 합니다.');}
+
+  set setter(int setmembers) {
+    if (setmembers > 0 && setmembers < 13) {
+      this._members = setmembers;
+    } else {
+      print('멤버 수는 1~12 사이어야 합니다.');
+    }
   }
 
-  void sayName(){
+  void sayName() {
     print('저는 $name입니다.');
   }
-  
-  void printSummary(){
-    print('그룹:${this.name}, 멤버 수: ${_members}');
+
+  void printSummary() {
+    print('그룹: ${this.name}, 멤버 수: ${_members}');
   }
 }
 
-class BoyGroup extends Idol{
+class BoyGroup extends Idol {
   BoyGroup(super.name, super._members);
-  void sayName(){
+
+  @override
+  void sayName() {
     print('저는 보이그룹 ${name}입니다.');
   }
 }
-void main(){
-  BoyGroup boygroup =BoyGroup("BTS",7)
-  ..sayName()
-  ..setter(13)
-  ..setter(6)
-  ..printSummary();
+
+void main() {
+  BoyGroup boygroup = BoyGroup("BTS", 7)
+    ..sayName()
+    ..setter = 13
+    ..setter = 6
+    ..printSummary();
 }
