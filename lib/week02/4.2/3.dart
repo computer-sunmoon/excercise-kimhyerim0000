d1fceba85a968c9fcb61632097412281c1fd1545
kimhy <kimhyyefla@gmail.com>
2025-09-11 13:19:44 +0900

    feat(week02/4.2): Add map destructuring example and comments

--- lib/week02/4.2/3.dart
+++ lib/week02/4.2/3.dart
@@ -1,6 +1,9 @@
 void main(){
   final newJeans={'minji':'민지', 'haerin':'해린'};
+  // 위의 맵의 구조와 똑같은 구조로 구조 분해하면 됩니다.
   final {'minji':minji,'haerin':haerin}=newJeans;
+  // name:민지
   print('name : $minji');
+  // name:해린
   print('name: $haerin');
 }
