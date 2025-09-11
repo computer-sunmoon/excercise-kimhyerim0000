2d829545275cf96732af9f27a2379eab177f8ec3
kimhy <kimhyyefla@gmail.com>
2025-09-11 13:15:45 +0900

    feat: Demonstrate spread operator to extract first, second, and last elements.

--- lib/week02/4.2/2.dart
+++ lib/week02/4.2/2.dart
@@ -1,7 +1,11 @@
 void main(){
   final numbers=[1,2,3,4,5,6,7,8,9];
+  // 스프레드 연산자를 사용하게 되면 중간의 값들을 버릴 수 있습니다.
   final [x,y,...,z]=numbers;
+  // 1 출력
   print(x);
+  // 2 출력
   print(y);
+  // 9 출력
   print(z);
 }
