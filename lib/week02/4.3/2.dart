8033d157000829084bf3b820304a68650fa6ed3d
kimhy <kimhyyefla@gmail.com>
2025-09-11 14:10:45 +0900

    feat: Add comments explaining switch case matching in 2.dart

--- lib/week02/4.3/2.dart
+++ lib/week02/4.3/2.dart
@@ -1,9 +1,10 @@
 void switcher(dynamic anything){
   switch(anything){
+    // 정확히 'aaa' 문자열만 매치합니다.
     case 'aaa':
     print('match:aaa');
     break;
-
+    // 정확히 [1,2] 리스트만 매치합니다.
     case [1,2]:
     print('match:[1,2]');
     break;
