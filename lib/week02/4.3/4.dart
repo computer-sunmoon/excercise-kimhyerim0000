cee4d0212296e88e6f1292cd1e81cf1520128d69
kimhy <kimhyyefla@gmail.com>
2025-09-11 14:28:15 +0900

    Fix: Remove unused variable names in record type

--- lib/week02/4.3/4.dart
+++ lib/week02/4.3/4.dart
@@ -1,11 +1,11 @@
 void main(){
-  (int a,int b) imsi=(1,-1);
+  (int ,int) imsi=(1,-1);
 
   switch(imsi){
     case (1,_) when imsi.$2>0:
     print('1,_');
     break;
-    
+
     default:
     print('default');
   }
