1237340a464899402f00b92e0438c2d4bad7e7c8
kimhy <kimhyyefla@gmail.com>
2025-09-11 13:11:14 +0900

    feat: Add named parameter Record example and empty file

--- lib/week02/4.1/5.dart
+++ lib/week02/4.1/5.dart
@@ -1,6 +1,9 @@
 void main(){
+  // 네임드 파라미터 형태로 REcored를 선언하는 방법입니다.
+  // 다른 네임드 파라미터와 마찬가지로 순서는 상관 없습니다.
   ({String name, int age}) minji=(name:'민지',age:19);
   print(minji.name);
   print(minji.age);
+  // (age:20, name:민지) 출력
   print(minji);
 }
