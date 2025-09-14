bfdf8c05f2a94309af94b1fa50d43cb032e4a1e1
kimhy <kimhyyefla@gmail.com>
2025-09-14 23:53:41 +0900

    feat: Illustrate typedef usage with function assignment and comments

--- lib/week01/1.7/3_2_typedef.dart
+++ lib/week01/1.7/3_2_typedef.dart
@@ -8,9 +8,12 @@ void subtract(int x,int y){
 }
 
 void main(){
+  // typedef는 일반적인 변수의 type처럼 사용 가능
   Operation oper= add;
   oper(1,2);
 
+// subtract()함수도 operation에 해당되는
+// 시그니처 반환값매개변수설정이므로 oper변수에 저장 가능
   oper=subtract;
   oper(1,2);
 }
