75bfd02e80f5c87ad777d607a7ed4db9a886851e
kimhy <kimhyyefla@gmail.com>
2025-09-15 09:40:10 +0900

    Updated Changes

--- lib/week02/5/main.dart
+++ lib/week02/5/main.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    
+    MaterialApp(
+      home: Scaffold(
+        body: Text(
+          'Hello Code Factory',
+        ),
+      ),
+    ),
   );
 }
