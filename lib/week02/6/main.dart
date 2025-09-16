fba14caf70df15fa607faf93bfd3877dcaf793c7
kimhy <kimhyyefla@gmail.com>
2025-09-16 14:00:30 +0900

    feat: Replace GestureDetector example with FloatingActionButtonExample

--- lib/week02/6/main.dart
+++ lib/week02/6/main.dart
@@ -1,35 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(FloatingActionButtonExample());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: GestureDetector(
-            onTap: (){
-              print('on tap');
-            },
-            onDoubleTap: (){
-              print('on double tap');
-            },
-            onLongPress: (){
-              print('on long press');
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
-              ),
-              width:100.0,
-              height: 100.0,
-            )
-          )
-        )
-      )
-    );
-  }
-}
