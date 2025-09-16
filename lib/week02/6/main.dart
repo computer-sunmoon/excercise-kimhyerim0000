be9c99bac6295de8af65ee172a50dd30477e2d79
kimhy <kimhyyefla@gmail.com>
2025-09-16 14:52:01 +0900

    feat: Comment out Scaffold widget in FloatingActionButtonExample

--- lib/week02/6/main.dart
+++ lib/week02/6/main.dart
@@ -8,26 +8,27 @@ class FloatingActionButtonExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold( // 전체화면을 차지한다. 원하는 부위는 SafeArea
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){ print('클릭'); },
-          child: Text('클릭'),
-          ),
-          body: Container( 
-            margin: EdgeInsets.all(16.0),
-            color: Colors.blue,
-            child: Padding(
-              padding: EdgeInsets.all(
-                16.0,
-              ),
-              child: Container(
-                color: Colors.red,
-                // width: 50.0,
-                // height: 50.0,
-              ),
-            ),
-          )
-        )
       );
     }
 }
+// Scaffold( // 전체화면을 차지한다. 원하는 부위는 SafeArea
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: (){ print('클릭'); },
+//           child: Text('클릭'),
+//           ),
+//           body: Container( 
+//             margin: EdgeInsets.all(16.0),
+//             color: Colors.blue,
+//             child: Padding(
+//               padding: EdgeInsets.all(
+//                 16.0,
+//               ),
+//               child: Container(
+//                 color: Colors.red,
+//                 // width: 50.0,
+//                 // height: 50.0,
+//               ),
+//             ),
+//           )
+//         )
+//       );
