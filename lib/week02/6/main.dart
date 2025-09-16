e370bdf6c437fb93e32db3927dc19763df1a9710
kimhy <kimhyyefla@gmail.com>
2025-09-16 13:51:25 +0900

    Updated Changes

--- lib/week02/6/main.dart
+++ lib/week02/6/main.dart
@@ -10,10 +10,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: IconButton(
-            onPressed: (){},
-            icon: Icon(
-              Icons.call,
+          child: GestureDetector(
+            onTap: (){
+              print('on tap');
+            },
+            onDoubleTap: (){
+              print('on double tap');
+            },
+            onLongPress: (){
+              print('on long press');
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              width:100.0,
+              height: 100.0,
             )
           )
         )
