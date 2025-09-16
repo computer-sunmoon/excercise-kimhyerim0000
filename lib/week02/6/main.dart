904edcab6b3066eb03eb3a31f39eb22b16969f44
kimhy <kimhyyefla@gmail.com>
2025-09-16 14:15:46 +0900

    feat: Remove unused container and SizedBox widgets

--- lib/week02/6/main.dart
+++ lib/week02/6/main.dart
@@ -13,25 +13,9 @@ class FloatingActionButtonExample extends StatelessWidget {
           onPressed: (){ print('클릭'); },
           child: Text('클릭'),
           ),
-          body: Container(   
-            decoration: BoxDecoration(
-              color: Colors.red,
-              border: Border.all(
-                width: 16.0,
-                color:Colors.black,
-              ),
-              borderRadius: BorderRadius.circular(
-                8.0,),
-            )
-          ),
-          SizedBox(
-            height: 200.0,
-            width:200.0,
-
-            child: Container(
-              color:Colors.red,
-            )
-          ),
+          body: Container( 
+            color: 
+          )
         )
       );
     }
