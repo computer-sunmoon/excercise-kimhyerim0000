0300a8a0792fffd0edf83e43e54aeab46c64c1b0
kimhy <kimhyyefla@gmail.com>
2025-09-16 13:13:05 +0900

    Fix: Remove empty child widget in Scaffold

--- lib/week02/6/main.dart
+++ lib/week02/6/main.dart
@@ -10,7 +10,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: 
+          child:
+          
+          )
         )
       )
     );
