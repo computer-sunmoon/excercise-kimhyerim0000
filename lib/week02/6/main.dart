75bec48e0ccd564b1104d267eb03a0daee54812e
kimhy <kimhyyefla@gmail.com>
2025-09-16 13:06:58 +0900

    Updated Changes

--- lib/week02/6/main.dart
+++ lib/week02/6/main.dart
@@ -8,7 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
-    )
+      home: Scaffold(
+        body: Center(
+          child: 
+        )
+      )
+    );
   }
 }
