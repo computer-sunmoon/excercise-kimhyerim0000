03a716f4a74d6be35d3b6f30a280c40d28a8fb0c
kimhy <kimhyyefla@gmail.com>
2025-09-17 12:13:16 +0900

    Fix: Remove incomplete Column widget

--- lib/week02/6/main.dart
+++ lib/week02/6/main.dart
@@ -11,27 +11,10 @@ class ColumnWidgetExample extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SizedBox(
-
           width: double.infinity,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
 
-            children: [
-              Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.red,
-              ),
-              const SizedBox(width: 12.0),
-              Container(
-                height: 50.0,
-                width: 
-              )
-
-            ]
-
-          )
+                      )
         )
       )
     )
