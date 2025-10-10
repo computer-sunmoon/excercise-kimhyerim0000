b6ccae2539992cb5178e98558a73503bbf578b5b
kimhy <kimhyyefla@gmail.com>
2025-10-10 22:24:58 +0900

    feat: Initialize date formatting for calendar

--- lib/week05/main.dart
+++ lib/week05/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:week03/week05/calendar_scheduler/screen/home_screen.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
   runApp(
     MaterialApp(
       home: HomeScreen(),
