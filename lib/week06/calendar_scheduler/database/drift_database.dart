2916e5d580d842a079263974f0bd21231ddc372e
kimhy <kimhyyefla@gmail.com>
2025-10-14 14:19:47 +0900

    Fix: Import path_provider package in drift_database.dart

--- lib/week06/calendar_scheduler/database/drift_database.dart
+++ lib/week06/calendar_scheduler/database/drift_database.dart
@@ -4,7 +4,7 @@ import 'package:drift/drift.dart';
 part 'drift_database.g.dart';
 
 import 'package:drift/native.dart';
-import 'packa'
+import 'package:path_provider/path_provider.dart';
 
 @DriftDatabase(
   tables: [
