6317735dd055031420ee7d0ccafa758b04da8167
kimhy <kimhyyefla@gmail.com>
2025-10-14 15:05:05 +0900

    fix: Register LocalDatabase singleton correctly

--- lib/week06/main.dart
+++ lib/week06/main.dart
@@ -3,12 +3,13 @@ import 'package:week03/week06/calendar_scheduler/screen/home_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:week03/week06/calendar_scheduler/database/drift_database.dart';
 import 'package:get_it/get_it.dart';
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting();
 
-  GetIt.I.registerSingleton<LocalDatabase> database;
+  GetIt.I.registerSingleton<LocalDatabase>(database);
   
   runApp(
     MaterialApp(
