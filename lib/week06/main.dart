95ad9c0b27d4fb91c0aa29ecb0068deaf8f9c4ef
kimhy <kimhyyefla@gmail.com>
2025-10-14 15:03:57 +0900

    feat: Register Drift database with GetIt

--- lib/week06/main.dart
+++ lib/week06/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:week03/week06/calendar_scheduler/screen/home_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:week03/week06/calendar_scheduler/database/drift_database.dart';
-
+import 'package:get_it/get_it.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
