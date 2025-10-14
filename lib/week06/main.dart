9f328620d008d8b776aaf71ad38948b36086f413
kimhy <kimhyyefla@gmail.com>
2025-10-14 14:59:22 +0900

    Fix: Update import path for drift_database.dart

--- lib/week06/main.dart
+++ lib/week06/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:week03/week06/calendar_scheduler/screen/home_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:myapp/week06/calendar_scheduler/database/drift_database.dart';
+import 'package:week03/week06/calendar_scheduler/database/drift_database.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
